d2e3a431979cb0c22223bee99db03566c5bfa47d
Daiki Yamazaki <daiki.yamazaki@noland-prod.com>
2023-02-12 23:19:59 +0900

    vue.js v3, TailwindCSS導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-import './bootstrap';
+// import './bootstrap';
+
+import { createApp } from 'vue/dist/vue.esm-bundler';
+
+const Counter = {
+    data() {
+        return {
+            counter: 0,
+        };
+    },
+    mounted() {
+        setInterval(() => {
+            this.counter++;
+        }, 1000);
+    },
+};
+
+createApp(Counter).mount("#counter");
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/vue', function () {
+    return view('vue');
 });
