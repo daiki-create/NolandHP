3432e36707d2a28921ddd8b7c12039d46040eeb2
Daiki Yamazaki <daiki.yamazaki@noland-prod.com>
2023-02-15 20:55:45 +0900

    ナビリンクから画面遷移＆共通レイアウト適用

---
+++ resources/js/Pages/Layouts/Layout.vue
@@ -0,0 +1,106 @@
+<template>
+    <v-app id="inspire">
+      <v-app-bar
+        class="px-3"
+        color="white"
+        flat
+        density="compact"
+      >
+        <img style="max-height: 100%" src="https://t3.ftcdn.net/jpg/04/06/91/62/360_F_406916265_hxmrv7wgw9SZN9871yebxQJAX7HsHdNp.jpg"/>
+  
+        <v-spacer></v-spacer>
+  
+        <nav>
+            <ul style="display:flex; align-items:center">
+                <li style="padding:0 20px;">
+                    <Link href="/">TOP</Link>
+                </li>
+                <li style="padding:0 20px;">
+                    <Link href="/about">ABOUT</Link>
+                </li>
+            </ul>
+        </nav>
+
+        <v-tabs
+          centered
+          color="grey-darken-2"
+        >
+        
+          <!-- <v-tab><Link href="/">TOP</Link></v-tab>
+          <v-tab><Link href="/about">ABOUT</Link></v-tab> -->
+          <!-- <v-tab href="#">TOP</v-tab>
+          <v-tab href="#about">ABOUT</v-tab> -->
+          <!-- <v-tab to="/">TOP</v-tab>
+          <v-tab to="/about">ABOUT</v-tab> -->
+        </v-tabs>
+      </v-app-bar>
+  
+      <v-main class="bg-grey-lighten-3">
+        <slot name="top-img"/>
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              sm="2"
+            >
+            </v-col>
+  
+            <v-col
+              cols="12"
+              sm="8"
+            >
+              <v-sheet
+                min-height="700vh"
+                rounded="lg"
+                class="pa-md-4 mx-lg-auto"
+              >
+                <slot/>
+              </v-sheet>
+            </v-col>
+  
+            <v-col
+              cols="12"
+              sm="2"
+            >
+              <v-sheet
+                rounded="lg"
+                min-height="268"
+                class="pa-md-2 mx-lg-auto"
+              >
+                おすすめ記事
+              </v-sheet>
+              <br>
+              <v-sheet
+                rounded="lg"
+                min-height="268"
+                class="pa-md-2 mx-lg-auto"
+              >
+                広告
+              </v-sheet>
+              <br>
+              <v-sheet
+                rounded="lg"
+                min-height="268"
+                class="pa-md-2 mx-lg-auto"
+              >
+                プロフィール
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script setup>
+    import { Link } from '@inertiajs/inertia-vue3';
+  </script>
+  
+  <script>
+    export default {
+        data: () => ({
+        
+        }),
+    }
+  </script>
+  
--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -1,85 +1,24 @@
 <template>
-    <v-app id="inspire">
-      <v-app-bar
-        class="px-3"
-        color="white"
-        flat
-        density="compact"
-      >
-        <!-- <v-avatar
-          color="grey-darken-1"
-          size="32"
-        ></v-avatar> -->
-  
-        <v-spacer></v-spacer>
-  
-        <v-tabs
-          centered
-          color="grey-darken-2"
-        >
-          <v-tab to="/">トップ</v-tab>
-          <v-tab to="/about">事業概要</v-tab>
-          <v-tab to="/blog">ブログ</v-tab>
-          <v-tab to="/contact">お問い合わせ</v-tab>
-        </v-tabs>
-        <!-- <v-spacer></v-spacer> -->
-  
-        <!-- <v-avatar
-          class="hidden-sm-and-down"
-          color="grey-darken-1"
-          size="32"
-        ></v-avatar> -->
-      </v-app-bar>
-  
-      <v-main class="bg-grey-lighten-3">
-        <v-container>
-          <v-row>
-            <v-col
-              cols="12"
-              sm="2"
-            >
-              <!-- <v-sheet
-                rounded="lg"
-                min-height="268"
-              >
-                test
-              </v-sheet> -->
-            </v-col>
-  
-            <v-col
-              cols="12"
-              sm="8"
-            >
-              <v-sheet
-                min-height="700vh"
-                rounded="lg"
-              >
-                <!--  -->
-              </v-sheet>
-            </v-col>
-  
-            <v-col
-              cols="12"
-              sm="2"
-            >
-              <v-sheet
-                rounded="lg"
-                min-height="268"
-              >
-                index
-              </v-sheet>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script>
+    <layout>
+        <template v-slot:top-img>
+            <div style="width: 100vw;height: 80vh; background-size: cover; background-image: url('/images/top-img.jpeg');"></div>
+        </template>
+        <template v-slot>
+            <h1>What's new</h1>
+        </template>
+    </layout>
+</template>
+
+<script>
+    import { InertiaLink } from '@inertiajs/inertia-vue3'
+    import Layout from './Layouts/Layout.vue'
+
     export default {
-      data: () => ({
-        
-      }),
+        data: () => ({
+        }),
+        components: {
+            Layout,
+        },
     }
-  </script>
+</script>
   
